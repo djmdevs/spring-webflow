b88461f02175a5e6136a1074f01663d004c439c6
djmdevs <djmdevs@gmail.com>
2020-06-18 21:32:49 +0200

    uc_003-Adding Controller and viewResolver configuration

--- src/main/java/mz/djm/sflow/fe/controller/LoginController.java
+++ src/main/java/mz/djm/sflow/fe/controller/LoginController.java
@@ -2,6 +2,7 @@ package mz.djm.sflow.fe.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,28 +13,34 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * @author Danilo Jo
  *
  */
+
+@Controller
+@RequestMapping("/lgc")
 public class LoginController {
 
 	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 	
-	@RequestMapping(value="/doGet")
+	@RequestMapping(value="/get")
 	public String doGet(ModelMap map, Model model) {
 		
 		//degub mode
 		logger.debug("Authentication Successful");
-		
-		//error mode
-		logger.error("Authentcation Failed NullPointer", new Exception("Testing"));
-		
+	
 		//sending message to specificy model
 		model.addAttribute("msg", "Success Loggin");
+		
+		//ResourceServlet r;
 		
 		return "login";
 	}
 	
-	@RequestMapping(value="/doPost", method = RequestMethod.POST)
+	@RequestMapping(value="/post", method = RequestMethod.POST)
 	public String doPost() {
-		return null;
+		
+		//error mode
+		logger.error("Authentcation Failed NullPointer", new Exception("Testing"));
+		
+		return "login";
 	}
 	
 	
