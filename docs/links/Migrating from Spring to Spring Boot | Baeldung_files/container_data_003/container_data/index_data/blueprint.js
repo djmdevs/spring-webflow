ef63f8e96cbbeebfcd37e5ce70bbc2950de1ae53
djmdevs <djmdevs@gmail.com>
2020-06-20 06:56:50 +0200

    Updating docs - Migration Spring to Sboot

---
+++ docs/links/Migrating from Spring to Spring Boot | Baeldung_files/container_data_003/container_data/index_data/blueprint.js
@@ -0,0 +1,192 @@
+window.blueprint = {
+	"width": 300,
+	"height": 250,
+	"images": [
+		"txt_1.png",
+		"txt_2.png",
+		"txt_3.png",
+		"cta_1.png",
+		"ctaHover_1.png",
+		"logo_vimeo.png"
+	],
+	"elements": {
+		"stage": {
+			"parent": "banner",
+			"id": "stage",
+			"retina": false,
+			"left": 0,
+			"top": 0,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 300,
+			"height": 250
+		},
+		"bg": {
+			"parent": "banner",
+			"id": "bg",
+			"retina": false,
+			"left": 0,
+			"top": 0,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 300,
+			"height": 250
+		},
+		"txt_1": {
+			"parent": "banner",
+			"id": "txt_1",
+			"retina": true,
+			"left": -11,
+			"top": 5,
+			"safeArea": 32,
+			"lineHeight": 35.84,
+			"backgroundImage": "txt_1.png",
+			"width": 304,
+			"height": 199
+		},
+		"txt_2": {
+			"parent": "banner",
+			"id": "txt_2",
+			"retina": true,
+			"left": -18,
+			"top": 0.625,
+			"safeArea": 39,
+			"lineHeight": 34.675,
+			"backgroundImage": "txt_2.png",
+			"width": 266,
+			"height": 178
+		},
+		"txt_3": {
+			"parent": "banner",
+			"id": "txt_3",
+			"retina": true,
+			"left": 5,
+			"top": 135,
+			"safeArea": 20,
+			"lineHeight": 16.64,
+			"backgroundImage": "txt_3.png",
+			"width": 287,
+			"height": 54
+		},
+		"cta": {
+			"parent": "banner",
+			"id": "cta",
+			"retina": true,
+			"left": 172.28753662109375,
+			"top": 196.25,
+			"safeArea": 0,
+			"lineHeight": 15.600000000000001
+		},
+		"cta_1": {
+			"parent": "cta",
+			"id": "cta_1",
+			"retina": true,
+			"backgroundImage": "cta_1.png"
+		},
+		"ctaHover": {
+			"parent": "banner",
+			"id": "ctaHover",
+			"retina": true,
+			"left": 194.28753662109375,
+			"top": 50.25,
+			"safeArea": 0,
+			"lineHeight": 15.600000000000001
+		},
+		"ctaHover_1": {
+			"parent": "ctaHover",
+			"id": "ctaHover_1",
+			"retina": true,
+			"backgroundImage": "ctaHover_1.png"
+		},
+		"logo_vimeo": {
+			"parent": "banner",
+			"id": "logo_vimeo",
+			"retina": true,
+			"left": 20,
+			"top": 200,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"backgroundImage": "logo_vimeo.png",
+			"width": 80,
+			"height": 23
+		},
+		"container_endframe": {
+			"parent": "banner",
+			"id": "container_endframe",
+			"retina": false,
+			"left": 20,
+			"top": 14,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 260,
+			"height": 180
+		},
+		"top": {
+			"parent": "banner",
+			"id": "top",
+			"retina": false,
+			"left": 0,
+			"top": -10,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 300,
+			"height": 10
+		},
+		"bottom": {
+			"parent": "banner",
+			"id": "bottom",
+			"retina": false,
+			"left": 0,
+			"top": 250,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 300,
+			"height": 10
+		},
+		"left": {
+			"parent": "banner",
+			"id": "left",
+			"retina": false,
+			"left": -10,
+			"top": 0,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 10,
+			"height": 250
+		},
+		"right": {
+			"parent": "banner",
+			"id": "right",
+			"retina": false,
+			"left": 300,
+			"top": 0,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 10,
+			"height": 250
+		}
+	},
+	"settings": {
+		"filename": "Q3-2019_PT_mid-funnel_marketer_unlimited-bandwidth_1_learn-more_300x250_HTML5",
+		"market": "PT",
+		"campaign": "Q3-2019",
+		"funnel": "mid-funnel",
+		"segment": "marketer",
+		"feature": "unlimited-bandwidth",
+		"copyVariation": "1",
+		"size": "300x250",
+		"marketIndex": "5",
+		"id": "marketerunlimited-bandwidth1",
+		"copy": {
+			"copy1": "Explicar largura de banda é difícil. Ilimitado não.",
+			"copy2": "Largura de Banda Ilimitada.",
+			"copy3": "Um Recurso do Player do Vimeo",
+			"copy4": "Saiba Mais",
+			"copy5": ""
+		},
+		"fileType": "jpg",
+		"fallback": true,
+		"HtmlfileType": "html",
+		"backgroundColor": "#8382c7"
+	}
+};
