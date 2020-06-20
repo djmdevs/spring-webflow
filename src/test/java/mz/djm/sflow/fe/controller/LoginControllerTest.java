bfa08b0b2b518117c8dc115c7d1c2c3bd909eea9
djmdevs <djmdevs@gmail.com>
2020-06-20 10:14:20 +0200

    Adding testing classes

---
+++ src/test/java/mz/djm/sflow/fe/controller/LoginControllerTest.java
@@ -0,0 +1,14 @@
+package mz.djm.sflow.fe.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LoginControllerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/mz/djm/sflow/fe/ws/ApplicationBootTest.java
@@ -0,0 +1,14 @@
+package mz.djm.sflow.fe.ws;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ApplicationBootTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
